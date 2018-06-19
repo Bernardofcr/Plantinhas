ae22732c5d9126b898498e1a1fba0640231debe4
Jonathan Coutinho A Santos <37001764+jsantos93@users.noreply.github.com>
2018-06-19 19:04:44 -0300

    Função de umidade e nível da agua

--- plantinea/plantinea.ino
+++ plantinea/plantinea.ino
@@ -3,15 +3,20 @@
 
 Servo myservo;
 
-const float p1 = 4.428;
+const float p1 = 4.428;   //Converte para mm 
 const float p2 = 16.17;
 const float p3 = 15.35;
 const float p4 = 7.229;
 const float p5 = 11.59;
 
 int a;
+int d;
+int y;
 float b;
 float w;
+
+int button = 7;
+int cont = 1;
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
@@ -38,76 +43,172 @@ byte half[8] = {
 };
 
 
-
+/* Solo umido - 0 a 400
+ * Solo Moderado - 400 - 800
+ * Solo seco - 800 - 1024
+ * 
+ * 
+ * 
+ * calculo para transformar a quantidade de agua no recipiente em litros.  
+ */
 
 
  
 void setup() {
   
   Serial.begin(9600);
-  myservo.attach(8);
+  myservo.attach(8,800,2500); // motor Servo
   lcd.begin(16, 2);
   lcd.createChar(1, full);
   lcd.createChar(2, half);
+  pinMode(7, INPUT);  // Nível da agua
+  pinMode(A1, INPUT); //Sensor de umidade
 
 }
 
 void loop() {
  
- a = analogRead(A0);
-
- b = (a - 641.1) / 83.3;
-
- w = (p1 * pow(b,4) + p2 * pow(b,3) + p3 * pow(b,2) + p4 * b + p5); 
-
-
-  if (a <= 480){ 
-    lcd.clear();
-    lcd.setCursor(1, 0);
-    lcd.print("Nivel de agua:");
-    lcd.setCursor(1, 1);
-    lcd.print("Vazio");
-    Serial.println("Nivel de água: 0mm - Vazio!"); 
-    myservo.write(180);
+  a = analogRead(A0);
+  d = analogRead(A1);
+  y = digitalRead(button);
+ 
+ if (y == HIGH){
+    delay(200);
+    cont++;
+    if ( cont > 3){
+      cont = 1;
+    }
+    status_planta(cont);
+    Serial.println(cont);
     delay(50);
-  
-  }
-  
-  else if (a > 710){ 
-    lcd.clear();
-    lcd.setCursor(1, 0);
-    lcd.print("Nivel de agua:");
-    Serial.println("Nivel de água: Cheio"); 
-    nivel_agua();
-    myservo.write(20);
+  } else {
+    status_planta(cont);
     delay(50);
   }
   
-  else { 
-    Serial.print("ADC: ");
-    Serial.println(a);
-    Serial.print("Nivel de água: ");
-    Serial.print(w);
-    Serial.println(" mm");
-  }
-  delay(1000);
+  
+  
 
 
 }
 
-void nivel_agua(){
-  for (int i = 4; i<11; i++){
+int nivel_agua(int agua){
+  /*for (int i = 4; i<11; i++){
     lcd.setCursor(i, 1);
     lcd.write((byte)1);
     delay(50);
     lcd.setCursor((i+1),1);
     lcd.write((byte)2);
     delay(50);
-  
+  */
+    b = (agua - 641.1) / 83.3;
+    w = (p1 * pow(b,4) + p2 * pow(b,3) + p3 * pow(b,2) + p4 * b + p5); // Pode ser transformado em cm ou m 
+
+    if (agua <= 480){ 
+   /* lcd.clear();
+      lcd.setCursor(1, 0);
+      lcd.print("Nivel de agua:");
+      lcd.setCursor(1, 1);
+      lcd.print("Vazio");
+      Serial.println("Nivel de água: 0mm - Vazio!"); 
+      myservo.write(180);
+      delay(50);*/
+      return 1;
   }
+
+    else if (agua > 710){ 
+      /*lcd.clear();
+      lcd.setCursor(1, 0);
+      lcd.print("Nivel de agua:");
+      Serial.println("Nivel de água: Cheio");
+      lcd.setCursor(1, 1);
+      lcd.print("Cheio"); 
+      myservo.write(0);
+      delay(50);*/
+      return 2;
+  }
+  
+    else { 
+      /*Serial.print("ADC: ");
+      Serial.println(agua);
+      Serial.print("Nivel de água: ");
+      Serial.print(w);
+      Serial.println(" mm");*/
+      return 3;
+  }
+
+
+  
 }
 
 
+  int umidade_solo(int solo ){
 
+    if (solo > 0 && solo < 400){
+        Serial.println("Solo umido");
+        return 1;
+    
+    } else if (solo > 400 && solo < 800){
+        Serial.println("Solo moderado");
+        return 2;
+    } else {
+        Serial.println("Solo Seco");
+        return 3;
+    }
+    
+}
+
+
+  int temperatura(){
+  
+}
+
+void status_planta(int lcd_value){
+  int p;
+
+ switch (lcd_value){
+
+    case 1: // Nivel de agua
+      lcd.clear();
+      lcd.setCursor(1, 0);
+      lcd.print("Nivel de agua:");
+      p = nivel_agua(a);
+      if (p == 1){
+        lcd.setCursor(1, 1);
+        lcd.print("Vazio"); 
+      } else if(p == 2){
+        lcd.setCursor(1, 1);
+        lcd.print("Cheio"); 
+      } else {
+        lcd.setCursor(1, 1);
+        lcd.print(w);
+     
+      }
+      break;
+      
+    case 2: // Umidade
+      lcd.clear();
+      lcd.setCursor(1, 0);
+      lcd.print("Umidade solo:");
+      p = umidade_solo(d);
+      if (p == 1){
+        lcd.setCursor(1, 1);
+        lcd.print("Umido"); 
+      } else if(p == 2){
+        lcd.setCursor(1, 1);
+        lcd.print("Moderado"); 
+      } else {
+        lcd.setCursor(1, 1);
+        lcd.print("Seco"); 
+      }
+      break;
+      
+    case 3: // Temperatura
+      lcd.clear();
+      lcd.setCursor(1, 0);
+      lcd.print("Temperatura:");
+      break;
+ }
+}
 
 
