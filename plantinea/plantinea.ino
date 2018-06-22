626b83a577e568437aee131b7c266823fa00d395
Jonathan Coutinho A Santos <37001764+jsantos93@users.noreply.github.com>
2018-06-22 13:57:39 -0300

    Update plantinea.ino

--- plantinea/plantinea.ino
+++ plantinea/plantinea.ino
@@ -11,15 +11,17 @@ const float p5 = 11.59;
 
 
 int a;
-int d;
-int y;
+int u;
+int b1;
+int b2;
 float b;
 float w;
 
 int luminoso;
 
 
-int button = 7;
+int button1 = 7;
+int button2 = 8;
 int cont = 1;
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
@@ -91,21 +93,9 @@ void status_planta (int lcd_value)
       lcd.clear();
       lcd.setCursor(1, 0);
       lcd.print("Umidade solo:");
-      p = umidade_solo(d);
-      /*
-      if (p == 1){
-        lcd.setCursor(1, 1);
-        lcd.print("Umido "+String(conversorpor100(d))); 
-      } else if(p == 2){
-        lcd.setCursor(1, 1);
-        lcd.print("Moderado "+String(conversorpor100(d))); 
-      } else {
-        lcd.setCursor(1, 1);
-        lcd.print("Seco "+String(conversorpor100(d))); 
-      }
-      */
+      p = umidade_solo(u);
       lcd.setCursor(1,1);
-      lcd.print(String(conversorpor100(d))+"%");
+      lcd.print(String(conversorpor100(u))+"%");
       break;
       
     case 3: // Luminosidade
@@ -130,13 +120,6 @@ void status_planta (int lcd_value)
         lcd.print("sem condicoes");
       }
       delay(500);
-
-      
-      /*
-      lcd.setCursor(1,1);
-      lcd.print(String(luminoso));
-      delay(1000);
-      */
       break;
  }
 }
@@ -145,7 +128,7 @@ void status_planta (int lcd_value)
 void setup() {
   
   Serial.begin(9600);
-  myservo.attach(8,800,2500); // motor Servo
+  myservo.attach(9,800,2500); // motor Servo
   lcd.begin(16, 2);
   lcd.createChar(1, full);
   lcd.createChar(2, half);
@@ -157,11 +140,13 @@ void setup() {
 void loop() {
  
   a = analogRead(A0);//nivel de agua
-  d = analogRead(A1);//umidade do solo
-  y = digitalRead(button);
+  u = analogRead(A1);//umidade do solo
+  b1 = digitalRead(button1);
+  b2 = digitalRead(button2);
+  
   luminoso=analogRead(A2);
  
- if (y == HIGH){
+ if (b1 == HIGH){
     delay(200);
     cont++;
     if ( cont > 3){
@@ -174,55 +159,27 @@ void loop() {
     status_planta(cont);
     delay(50);
   }
-  
-  
+
+  teste_door(b2);
   
 
 
 }
 
 int nivel_agua(int agua){
-  /*for (int i = 4; i<11; i++){
-    lcd.setCursor(i, 1);
-    lcd.write((byte)1);
-    delay(50);
-    lcd.setCursor((i+1),1);
-    lcd.write((byte)2);
-    delay(50);
-  */
+ 
     b = (agua - 641.1) / 83.3;
     w = (p1 * pow(b,4) + p2 * pow(b,3) + p3 * pow(b,2) + p4 * b + p5); // Pode ser transformado em cm ou m 
 
     if (agua <= 480){ 
-   /* lcd.clear();
-      lcd.setCursor(1, 0);
-      lcd.print("Nivel de agua:");
-      lcd.setCursor(1, 1);
-      lcd.print("Vazio");
-      Serial.println("Nivel de água: 0mm - Vazio!"); 
-      myservo.write(180);
-      delay(50);*/
       return 1;
   }
 
     else if (agua > 710){ 
-      /*lcd.clear();
-      lcd.setCursor(1, 0);
-      lcd.print("Nivel de agua:");
-      Serial.println("Nivel de água: Cheio");
-      lcd.setCursor(1, 1);
-      lcd.print("Cheio"); 
-      myservo.write(0);
-      delay(50);*/
       return 2;
   }
   
     else { 
-      /*Serial.print("ADC: ");
-      Serial.println(agua);
-      Serial.print("Nivel de água: ");
-      Serial.print(w);
-      Serial.println(" mm");*/
       return 3;
   }
 
@@ -271,6 +228,40 @@ int nivel_agua(int agua){
     }
     }
 
+  void open_door(){
+    int c = 0;
+    while (c <= 180){
+      myservo.write(c);
+      delay(50);
+      c ++;
+    }
+     
+  }
 
 
+  void close_door(){
+    int c = 180;
+    while (c > 1){
+      myservo.write(c);
+      delay(50);
+      c --;
+    }
+  }
 
+  void teste_door(int status_button){
+    if (status_button == HIGH){
+      int c = 0;
+      while (c <= 180){
+        myservo.write(c);
+        delay(50);
+        c ++;
+      }
+       if (c = 181){
+        while (c > 1){
+          myservo.write(c);
+          delay(50);
+          c --;
+        }
+       }
+    }   
+  }
